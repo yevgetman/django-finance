83284effc7c3e632a40a1f4caae7d23ca51eee7b
Yevgeny Getman <yevgetman@gmail.com>
2025-06-03 15:43:24 -0500

    add recommendations endpoint

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('analyze/', views.analyze_portfolio, name='analyze-portfolio'),
+    path('recommendations/', views.get_portfolio_recommendations, name='portfolio-recommendations'),
     path('ticker-info/', views.get_ticker_info, name='ticker-info'),
 ]
