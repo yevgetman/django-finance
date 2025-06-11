08231a975b062895947016c2d6a7d0cfb3227a06
Yevgeny Getman <yevgetman@gmail.com>
2025-06-11 13:40:14 -0500

    Fix: Remove unused import causing deployment failure

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from .permissions import GlobalHardcodedAPIKeyPermission
 import yfinance as yf
 import concurrent.futures
 import os
